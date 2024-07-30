be0c4a232283c555e12e0ed93e6e2e136c648a44
Maipham72 <hongmai.pham@student.adelaide.edu.au>
2024-07-30 23:03:36 +0930

    add

--- even.c
+++ even.c
@@ -3,42 +3,38 @@
 #include <signal.h>
 #include <stdlib.h>
 
-volatile sig_atomic_t stop = 0;
-
-void printEven(int n) {
-  for (int i = 0; i <= n; i++) {
-    if (i % 2 == 0) {
-      printf("%d\n", i);
-      sleep(5);
-    }
-  }
-}
+// void printEven(int n) {
+//   for (int i = 0; i <= n; i++) {
+//     if (i % 2 == 0) {
+//       printf("%d\n", i);
+//       sleep(5);
+//     }
+//   }
+// }
 
 void handleSighup(int sig) {
   printf("Ouch!\n");
+  fflush(stdout);
 }
 
 void handleSigint(int sig) {
   printf("Yeah!\n");
-  stop = 1;
+  fflush(stdout);
 }
 
 int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    fprintf(stderr, "Usage: %s <number>\n", argv[0]);
-    return 1;
-  }
-
   int n = atoi(argv[1]);
 
   signal(SIGHUP, handleSighup);
   signal(SIGINT, handleSigint);
 
-  printEven(n);
-  
-  for (int i = 0; i < 30; i++) {
-    if (stop) break;
-    sleep(1);
+  // printEven(n);
+
+  for (int i = 0; i <= n; i++) {
+    if (i % 2 == 0) {
+      printf("%d\n", i);
+      sleep(5);
+    }
   }
 
   // while (!stop) {
