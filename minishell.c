99e6d0a36c90401a41c7fc3a1361935a7aaad491
Maipham72 <hongmai.pham@student.adelaide.edu.au>
2024-08-04 18:30:03 +0930

    work pls

--- minishell.c
+++ minishell.c
@@ -46,8 +46,12 @@ int main(int argk, char *argv[], char *envp[])
 
   while (1) { /* do Forever */
     prompt();
-    fgets(line, NL, stdin);
-    fflush(stdin);
+    if (!fgets(line, NL, stdin)) {
+      perror("fgets error");
+      exit(1);
+    }
+    // fgets(line, NL, stdin);
+    // fflush(stdin);
 
     if (feof(stdin)) { /* non-zero on EOF  */
 
@@ -58,12 +62,6 @@ int main(int argk, char *argv[], char *envp[])
     if (line[0] == '#' || line[0] == '\n' || line[0] == '\000')
       continue; /* to prompt */
 
-    bg = 0;
-    if (line[strlen(line) - 2] == '&') {
-      bg = 1;
-      line[strlen(line) - 2] = '\0';
-    }
-
     v[0] = strtok(line, sep);
     for (i = 1; i < NV; i++) {
       v[i] = strtok(NULL, sep);
@@ -71,48 +69,62 @@ int main(int argk, char *argv[], char *envp[])
     }
     /* assert i is number of tokens + 1 */
 
-    /* fork a child process to exec the command in v[0] */
+    bg = 0;
+    if (i > 1 && strcmp(v[i-1], "&") == 0) {
+      bg = 1;
+      v[i-1] = NULL;
+    }
 
     if (strcmp(v[0], "cd") == 0) {
-      if (v[1] == NULL) {
-        fprintf(stderr, "cd: missing arg\n");
-      } else {
-        size_t len = strlen(v[1]);
-        if (len > 0 && v[1][len - 1] == '\n') {
-          v[1][len - 1] = '\0';
-        }
-
-        printf("Changing directory to: %s\n", v[1]);
-
-        if (chdir(v[1]) != 0) {
-          perror("chdir");
-        }
+      if (v[1] == NULL || chdir(v[1]) != 0) {
+        perror("cd error");
       }
       continue;
-    } else if (strcmp(v[0], "exit") == 0) {
-      exit(0);
     }
+
+    /* fork a child process to exec the command in v[0] */
+    // if (strcmp(v[0], "cd") == 0) {
+    //   if (v[1] == NULL) {
+    //     fprintf(stderr, "cd: missing arg\n");
+    //   } else {
+    //     size_t len = strlen(v[1]);
+    //     if (len > 0 && v[1][len - 1] == '\n') {
+    //       v[1][len - 1] = '\0';
+    //     }
+
+    //     printf("Changing directory to: %s\n", v[1]);
+
+    //     if (chdir(v[1]) != 0) {
+    //       perror("chdir");
+    //     }
+    //   }
+    //   continue;
+    // } else if (strcmp(v[0], "exit") == 0) {
+    //   exit(0);
+    // }
 
     switch (frkRtnVal = fork()) {
       case -1: /* fork returns error to parent process */
       {
-        perror("fork");
+        perror("fork error");
         break;
       }
       case 0: /* code executed only by child process */
       {
         if (execvp(v[0], v) == -1) {
-          perror("execvp");
-          exit(1);
+          perror("execvp error");
+          exit(EXIT_FAILURE);
         }
         break;
       }
       default: /* code executed only by parent process */
-        if (!bg) {
-          wpid = wait(0);
-          printf("%d done\n", wpid);
+        if (bg == 0) {
+          if (waitpid(frkRtnVal, NULL, 0) == -1) {
+            perror("waitpid error");
+          }
+          printf("%s done \n", v[0]);
         } else {
-          printf("%s started in bg\n", v[0]);
+          printf("Process %d running in background\n", frkRtnVal);
         }
         break;
     } /* switch */
