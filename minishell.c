6b702080365cc41c48a518e5619ccd5b72281377
Maipham72 <hongmai.pham@student.adelaide.edu.au>
2024-08-04 18:32:57 +0930

    work pls

--- minishell.c
+++ minishell.c
@@ -35,7 +35,7 @@ int main(int argk, char *argv[], char *envp[])
 
 {
   int frkRtnVal;       /* value returned by fork sys call */
-  int wpid;            /* value returned by wait */
+  // int wpid;            /* value returned by wait */
   char *v[NV];         /* array of pointers to command line tokens */
   char *sep = " \t\n"; /* command line token separators    */
   int i;               /* parse index */
