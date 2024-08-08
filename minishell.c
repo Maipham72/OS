8e8d8c427ef81faf4e400813747156ea00b6a83e
Maipham72 <hongmai.pham@student.adelaide.edu.au>
2024-08-08 12:48:45 +0930

    add

--- minishell.c
+++ minishell.c
@@ -1,141 +1,109 @@
 /*********************************************************************
    Program  : miniShell                   Version    : 1.3
  --------------------------------------------------------------------
-   skeleton code for linix/unix/minix command line interpreter
+   skeleton code for linux/unix/minix command line interpreter
  --------------------------------------------------------------------
-   File			: minishell.c
-   Compiler/System	: gcc/linux
+   File                : minishell.c
+   Compiler/System     : gcc/linux
 
 ********************************************************************/
 
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <stdio.h>
+#include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <errno.h>
 
-#define NV 20  /* max number of command tokens */
-#define NL 100 /* input buffer size */
-char line[NL]; /* command input buffer */
+#define NV 20            /* max number of command tokens */
+#define NL 100           /* input buffer size */
+char line[NL];   /* command input buffer */
 
-/*  shell prompt
-*/
-
-typedef struct {
-  int pid;
-  int index;
-  char command[NL];
-} BackgroundProcess;
+int bg_process_count = 0;
 
 void prompt(void) {
-  // fprintf(stdout, "\n msh> ");
-  fflush(stdout);
+    // fprintf(stdout, "\n msh> ");
+    fflush(stdout);
 }
- 
-int main(int argk, char *argv[], char *envp[])
-/* argk - number of arguments */
-/* argv - argument vector from command line */
-/* envp - environment pointer */
 
-{
-  int frkRtnVal;       /* value returned by fork sys call */
-  // int wpid;            /* value returned by wait */
-  char *v[NV];         /* array of pointers to command line tokens */
-  char *sep = " \t\n"; /* command line token separators    */
-  int i;               /* parse index */
-
-  int bg; /* background flag */
-
-  BackgroundProcess bgProcesses[NV];
-  int bgCount = 0;
-  int jobNumber = 1;
-  /* prompt for and process one command line at a time  */
-
-  while (1) { /* do Forever */
-    prompt();
-    if (!fgets(line, NL, stdin)) {
-      perror("fgets error");
-      exit(1);
+void handle_cd(char *path) {
+    if (chdir(path) == -1) {
+        perror("chdir");
     }
-    // fgets(line, NL, stdin);
-    // fflush(stdin);
+}
 
-    if (feof(stdin)) { /* non-zero on EOF  */
-
-      // fprintf(stderr, "EOF pid %d feof %d ferror %d\n", getpid(), feof(stdin),
-      //         ferror(stdin));
-      exit(0);
+void handle_background_processes() {
+    int status;
+    pid_t pid;
+    while ((pid = waitpid(-1, &status, WNOHANG)) > 0) {
+        bg_process_count--;
+        printf("[%d]+ Done pid %d\n", bg_process_count + 1, pid);
     }
-    if (line[0] == '#' || line[0] == '\n' || line[0] == '\000')
-      continue; /* to prompt */
+}
 
-    v[0] = strtok(line, sep);
-    for (i = 1; i < NV; i++) {
-      v[i] = strtok(NULL, sep);
-      if (v[i] == NULL) break;
-    }
-    /* assert i is number of tokens + 1 */
+int main(int argc, char *argv[], char *envp[]) {
+    int frkRtnVal;    /* value returned by fork sys call */
+    // int wpid;         /* value returned by wait */
+    char *v[NV];      /* array of pointers to command line tokens */
+    char *sep = " \t\n";/* command line token separators */
+    int i;
 
-    bg = 0;
-    if (i > 1 && strcmp(v[i-1], "&") == 0) {
-      bg = 1;
-      v[i-1] = NULL;
-    }
-
-    if (strcmp(v[0], "cd") == 0) {
-      if (v[1] == NULL || chdir(v[1]) != 0) {
-        perror("cd error");
-      }
-      continue;
-    }
-
-    /* fork a child process to exec the command in v[0] */
-    for (int j = 0; j < bgCount;) {
-      int status;
-      pid_t result = waitpid(bgProcesses[i].pid, &status, WNOHANG);
-      if (result != 0) {
-        printf("[%d]+ Done\t\t%s\n", bgProcesses[j].index,bgProcesses[j].command);
-        // for (int k = j; k < bgCount - 1; k++) {
-        bgProcesses[j] = bgProcesses[--bgCount];
-        // }
-        // bgCount--;
-        // j--;
-      } else {
-        j++;
-      }
-    }
-
-    switch (frkRtnVal = fork()) {
-      case -1: /* fork returns error to parent process */
-      {
-        perror("fork error");
-        break;
-      }
-      case 0: /* code executed only by child process */
-      {
-        if (execvp(v[0], v) == -1) {
-          perror("execvp error");
-          exit(EXIT_FAILURE);
+    while (1) {
+        prompt();
+        if (fgets(line, NL, stdin) == NULL) {
+            if (feof(stdin)) {
+                // fprintf(stderr, "EOF pid %d feof %d ferror %d\n", getpid(), feof(stdin), ferror(stdin));
+                exit(0);
+            }
+            continue;
         }
-        break;
-      }
-      default: /* code executed only by parent process */
-        if (bg == 0) {
-          if (waitpid(frkRtnVal, NULL, 0) == -1) {
-            perror("waitpid error");
-          }
-          // printf("%s done \n", v[0]);
-        } else {
-          bgProcesses[bgCount].pid = frkRtnVal;
-          bgProcesses[bgCount].index = jobNumber++;
-          snprintf(bgProcesses[bgCount].command, NL, "%s", v[0]);
-          printf("[%d] %d\n", bgProcesses[bgCount].index, frkRtnVal);
-          bgCount++;
+
+        if (line[0] == '#' || line[0] == '\n' || line[0] == '\000')
+            continue;   /* to prompt */
+
+        v[0] = strtok(line, sep);
+        for (i = 1; i < NV; i++) {
+            v[i] = strtok(NULL, sep);
+            if (v[i] == NULL)
+                break;
         }
-        break;
-    } /* switch */
-  } /* while */
-  return 0;
-} /* main */
+
+        if (strcmp(v[0], "cd") == 0) {
+            handle_cd(v[1]);
+            continue;
+        }
+
+        int is_background = (v[i-1] && strcmp(v[i-1], "&") == 0);
+        if (is_background) {
+            v[i-1] = NULL;  // Remove the '&' from arguments
+        }
+
+        switch (frkRtnVal = fork()) {
+        case -1:    /* fork error */
+            perror("fork");
+            break;
+
+        case 0:     /* child process */
+            if (execvp(v[0], v) == -1) {
+                perror("execvp");
+                exit(errno);
+            }
+            break;
+
+        default:    /* parent process */
+            if (is_background) {
+                bg_process_count++;
+                printf("[%d] %d\n", bg_process_count, frkRtnVal);
+            } else {
+                if (waitpid(frkRtnVal, NULL, 0) == -1) {
+                    perror("waitpid");
+                }
+            }
+            break;
+        }
+
+        handle_background_processes();
+    }
+}
